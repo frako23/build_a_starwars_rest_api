351d5240bb26c4ae44bb393b2f57a503b0fff215
frako23 <frako23@gmail.com>
2022-11-02 23:40:17 +0000

    Se agrega el proyecto

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,35 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(250),unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __init__(self, **kwargs):
+        self.full_name = kwargs['full_name']
+        self.email = kwargs['email']
+        self.password = kwargs['password']
+
+    @classmethod
+    def create(cls, **kwargs):
+        new_user = cls(**kwargs)
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+            return new_user
+        except Exception as error:
+            raise Exception(error.args[0], 400)
+        print(new_user.id)
+        return new_user
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +38,201 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "full_name": self.full_name,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(50))
+    hair_color = db.Column(db.String(50))
+    skin_color = db.Column(db.String(50))
+
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.gender = kwargs['gender']
+        self.hair_color = kwargs['hair_color']
+        self.skin_color = kwargs['skin_color']
+    
+    @classmethod
+    def create(cls, **kwargs):
+        new_character = cls(**kwargs)
+        db.session.add(new_character)
+        try:
+            db.session.commit()
+            return new_character
+        except Exception as error:
+            raise Exception(error.args[0], 400)
+            print(new_character.id)
+        return new_character
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.String(250))
+    climate = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
+
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.population = kwargs['population']
+        self.climate = kwargs['climate']
+        self.terrain = kwargs['terrain']
+
+    @classmethod
+    def create(cls, **kwargs):
+        new_planet = cls(**kwargs)
+        db.session.add(new_planet)
+        try:
+            db.session.commit()
+            return new_planet
+        except Exception as error:
+            raise Exception(error.args[0],400)
+            print(new_planet.id)
+            return new_planet
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    model = db.Column(db.String(50))
+    manufacturer = db.Column(db.String(250))
+    crew = db.Column(db.Integer)
+    passengers = db.Column(db.Integer)
+
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.model = kwargs['model']
+        self.manufacturer = kwargs['manufacturer']
+        self.crew = kwargs['crew']
+        self.passengers = kwargs['passengers']
+    
+    @classmethod
+    def create(cls, **kwargs):
+        new_vehicle = cls(**kwargs)
+        db.session.add(new_vehicle)
+        try:
+            db.session.commit()
+            return new_vehicle
+        except Exception as error:
+            raise Exception(error.args[0], 400)
+        print(new_vehicle.id)
+        return new_vehicle
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "crew": self.crew,
+            "passengers": self.passengers
+        }
+
+class CharacterFav(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    character_id = db.Column(Integer, ForeignKey('character.id'))
+    user_id = db.Column(db.Integer,ForeignKey ('user.id') )
+
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.character_id = kwargs['character_id']
+        self.user_id = kwargs['user_id']
+
+    @classmethod
+    def create(cls, **kwargs):
+        new_characterfav = cls(**kwargs)
+        db.session.add(new_characterfav)
+        try:
+            db.session.commit()
+            return new_characterfav
+        except Exception as error:
+            raise Exception(error.args[0], 400)
+        print(new_characterfav.id)
+        return new_characterfav
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "character_id": self.character_id,
+            "user_id": self.user_id
+        }    
+
+class PlanetFav(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    planet_id = db.Column(db.Integer, ForeignKey('planet.id'))
+    user_id = db.Column(db.Integer,ForeignKey ('user.id') )
+
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.planet_id = kwargs['planet_id']
+        self.user_id = kwargs['user_id']
+        
+    @classmethod
+    def create(cls, **kwargs):
+        new_planetfav = cls(**kwargs)
+        db.session.add(new_planetfav)
+        try:
+            db.session.commit()
+            return new_planetfav
+        except Exception as error:
+            raise Exception(error.args[0], 400)
+        print(new_planetfav.id)
+        return new_planetfav
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "planet_id": self.planet_id,
+            "user_id": self.user_id
+        }   
+
+class VehicleFav(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    vehicle_id = db.Column(db.Integer, ForeignKey('vehicle.id'))
+    user_id = db.Column(db.Integer,ForeignKey ('user.id') )
+
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.vehicle_id = kwargs['vehicle_id']
+        self.user_id = kwargs['user_id']
+
+    @classmethod
+    def create(cls, **kwargs):
+        new_vehiclefav = cls(**kwargs)
+        db.session.add(new_vehiclefav)
+        try:
+            db.session.commit()
+            return new_vehiclefav
+        except Exception as error:
+            raise Exception(error.args[0], 400)
+        print(new_vehiclefav.id)
+        return new_vehiclefav
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "vehicle_id": self.vehicle_id,
+            "user_id": self.user_id
+        }  
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,174 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Character, Planet, Vehicle, CharacterFav, PlanetFav, VehicleFav
 from api.utils import generate_sitemap, APIException
+import requests
 
 api = Blueprint('api', __name__)
 
+users = []
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/users', methods=['GET','POST'])
+def get_users():
+    if request.method == 'GET':
+        users = User.query.all()
+        users_dictionaries = []
+        for user in users:
+            users_dictionaries.append(user.serialize())
+        return jsonify(users_dictionaries), 200
+    new_user_data = request.json
+    try:
+        new_user = User.create(**new_user_data)
+        return jsonify(new_user.serialize()),201
+    except Exception as error:
+        return jsonify(error.args[0]), error.args[1]
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+characters = []
 
-    return jsonify(response_body), 200
+@api.route('/people', methods=['GET','POST'])
+def get_characters():
+    if request.method == 'GET':
+        characters = Character.query.all()
+        characters_dictionaries = []
+        for character in characters:
+            characters_dictionaries.append(character.serialize())
+        return jsonify(characters_dictionaries), 200
+    new_character_data = request.json
+    try:
+        new_character = Character.create(**new_character_data)
+        return jsonify(new_character.serialize()),201
+    except Exception as error:
+        return jsonify(error.args[0]), error.args[1]
+
+planets = []
+
+@api.route('/planets', methods=['GET', 'POST'])
+def get_planets():
+    if request.method == 'GET':
+        planets = Planet.query.all()
+        planets_dictionaries = []
+        for planet in planets:
+            planets_dictionaries.append(planet.serialize())
+        return jsonify(planets_dictionaries), 200
+    new_planet_data = request.json
+    try:
+        new_planet = Planet.create(**new_planet_data)
+        return jsonify(new_planet.serialize()), 201
+    except Exception as error:
+        return jsonify(error.args[0]), error.args[1]
+
+vehicles = []
+
+@api.route('/vehicles', methods=['GET', 'POST'])
+def get_vehicles():
+    if request.method == 'GET':
+        vehicles = Vehicle.query.all()
+        vehicles_dictionaries = []
+        for vehicle in vehicles:
+            vehicles_dictionaries.append(vehicle.serialize())
+        return jsonify(vehicles_dictionaries), 200
+    new_vehicle_data = request.json
+    try:
+        new_vehicle = Vehicle.create(**new_vehicle_data)
+        return jsonify(new_vehicle.serialize()), 201
+    except Exception as error:
+        return jsonify(error.args[0]), error.args[1]
+
+favorites = []
+
+@api.route('/users/<int:user_id>/favorites', methods=['GET'])
+def get_favorites(user_id):
+    if request.method == 'GET':
+        favorites_characters = CharacterFav.query.all()
+        favorites_planets = PlanetFav.query.all()
+        favorites_vehicles = VehicleFav.query.all()
+        favorites_dictionaries = []
+        for character in favorites_characters:
+            favorites_dictionaries.append(character.serialize())
+        for planet in favorites_planets:
+            favorites_dictionaries.append(planet.serialize())
+        for vehicle in favorites_vehicles:
+            favorites_dictionaries.append(vehicle.serialize())
+        return jsonify(favorites_dictionaries), 200
+
+@api.route('/favorite/people/<int:user_id>/<int:people_id>', methods=['POST', 'DELETE'])
+def add_people_favorites(user_id, people_id):
+    if request.method == 'POST':
+        new_people_favorite = request.json
+        try:
+            new_people_favorite = CharacterFav.create(user_id = user_id, character_id = people_id, **new_people_favorite)
+            return jsonify(new_people_favorite.serialize()), 201
+        except Exception as error:
+            return jsonify(error.args[0]), 500
+    else:
+        variable = CharacterFav.query.filter_by(user_id=user_id, character_id=people_id).delete()
+        try:
+            if variable == 0:
+                raise Exception("no hay favoritos", 404)
+            db.session.commit()
+            return jsonify({"mensaje": "Se elimino correctamente"})
+        except Exception as error:
+            return jsonify(error.args[0]), error.args[1]
+
+@api.route('/favorite/planets/<int:user_id>/<int:planet_id>', methods=['POST', 'DELETE'])
+def add_planets_favorites(user_id, planet_id):
+    if request.method == 'POST':
+        new_planet_favorite = request.json
+        try:
+            new_planet_favorite = PlanetFav.create(user_id = user_id, planet_id = planet_id, **new_planet_favorite)
+            return jsonify(new_planet_favorite.serialize()),201
+        except Exception as error:
+            return jsonify(error.args[0]), 500
+    else:
+        variable = PlanetFav.query.filter_by(user_id=user_id, planet_id=planet_id).delete()
+        try:
+            if variable == 0:
+                raise Exception("no hay favoritos", 404)
+            db.session.commit()
+            return jsonify({"mensaje": "Se elimino correctamente"})
+        except Exception as error:
+                return jsonify(error.args[0]), error.args[1]
+
+@api.route('/favorite/vehicles/<int:user_id>/<int:vehicle_id>', methods=['POST', 'DELETE'])
+def add_vehicles_favorites(user_id, vehicle_id):
+    if request.method == 'POST':
+        new_vehicle_favorite = request.json
+        try:
+            new_vehicle_favorite = VehicleFav.create(user_id = user_id, vehicle_id = vehicle_id, **new_vehicle_favorite)
+            return jsonify(new_vehicle_favorite.serialize()),201
+        except Exception as error:
+            return jsonify(error.args[0]), 500
+    else:
+        variable = VehicleFav.query.filter_by(user_id = user_id, vehicle_id = vehicle_id).delete()
+        try:
+            if variable == 0:
+                raise Exception("no hay favoritos", 404)
+            db.session.commit()
+            return jsonify({"mensaje": "Se elimino correctamente"})
+        except Exception as error:
+                    return jsonify(error.args[0]), error.args[1]
+
+@api.route('/input_data/planets', methods=['POST'])
+def input_data_planets():
+    response = requests.get('https://swapi.dev/api/planets')
+    data = response.json()
+    planets = Planet.query.all()
+    if len(planets) == data['count']:
+        return jsonify("Los planetas ya existen"), 400
+    
+    null = False
+    def query():
+        count = 0
+        for number in range(len(data['results'])):
+            Planet.create(name = data['results'][number]['name'], population = data['results'][number]['population'], climate = data['results'][number]['climate'], terrain = data['results'][number]['terrain'])
+        count = count + 1
+        return query()
+    
+    while not null:
+        response = requests.get(data['next'])
+        data = response.json()
+        query()
+        null = data['next'] is None
+
+    return jsonify(f'Se han creado {count} planetas') 
